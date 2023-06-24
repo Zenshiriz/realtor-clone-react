ea78a2ae007afe4d08dd9d471291e1047acbe4f2
mohammad riza <abdulrazakumer@gmail.com>
2023-06-24 18:03:15 +0530

    creating react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router , Routes ,Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUP from './pages/SignUP';
+import ForgotPassword from './pages/ForgotPassword';
+import Offers from './pages/Offers';
 
 function App() {
   return (
-    <div className="App">
-     <h1>hello world</h1>
-    </div>
+    <>
+   <Router>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/profile' element={<Profile/>}/>
+      <Route path='/sign-in' element={<SignIn/>}/>
+      <Route path='/sign-up' element={<SignUP/>}/>
+      <Route path='/forgot-password' element={<ForgotPassword/>}/>
+      <Route path='/offers' element={<Offers/>}/>
+    </Routes>
+   </Router>
+    </>
   );
 }
 
