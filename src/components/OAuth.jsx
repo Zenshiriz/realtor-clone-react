5f033f687f6212c9e835bc74984be52eb671f7d2
mohammad riza <abdulrazakumer@gmail.com>
2023-06-27 18:33:58 +0530

    made google signup

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,10 +1,39 @@
-import {FcGoogle} from
-"react-icons/fc"
+import { FcGoogle } from "react-icons/fc";
+import {getAuth, GoogleAuthProvider, signInWithPopup} from "firebase/auth"
+import { toast } from "react-toastify";
+import { db } from "../firebase";
+import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
+import { useNavigate } from "react-router";
+
 export default function OAuth() {
+  const navigate = useNavigate()
+ async function onGoogleClick(event) {
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth , provider);
+      const user = result.user
+
+      //check for the user
+      const docRef = doc(db,"users",user.uid)
+     const docSnap = await getDoc(docRef)
+        
+     if(!docSnap.exists()){
+      setDoc(docRef,{name : user.displayName , email : user.email , timestamp : serverTimestamp()})
+     }
+     navigate("/")
+    } catch (error) {
+      toast.error("could not authorize with google");
+    }
+  }
   return (
-    <button className="flex justify-center items-center w-full bg-red-700 text-white px-7 py-3 uppercase text-sm font-medium hover:bg-red-800 active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg transition-all duration-150 ease-in-out rounded">
-        <FcGoogle className=" text-2xl bg-white rounded-full mr-2"/>
-        Continue with Google
+    <button
+      type="button"
+      className="flex justify-center items-center w-full bg-red-700 text-white px-7 py-3 uppercase text-sm font-medium hover:bg-red-800 active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg transition-all duration-150 ease-in-out rounded"
+      onClick={onGoogleClick}
+    >
+      <FcGoogle className=" text-2xl bg-white rounded-full mr-2" />
+      Continue with Google
     </button>
   );
 }
