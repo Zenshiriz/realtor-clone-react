326fec7a14924dd36a566ad76dc469bfa2f5924c
mohammad riza <abdulrazakumer@gmail.com>
2023-07-06 18:02:05 +0530

    create listing page ui

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,13 +3,14 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router";
 import { toast } from "react-toastify";
 import { db } from "../firebase";
-import { doc, updateDoc} from "firebase/firestore";
-
+import { doc, updateDoc } from "firebase/firestore";
+import { FcHome } from "react-icons/fc";
+import { Link } from "react-router-dom";
 
 export default function Profile() {
   const auth = getAuth();
-  const navigate = useNavigate()
-  const [changeDetail , setChangeDetail] = useState(false)
+  const navigate = useNavigate();
+  const [changeDetail, setChangeDetail] = useState(false);
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email,
@@ -18,32 +19,32 @@ export default function Profile() {
 
   function onLogOut() {
     auth.signOut();
-    navigate("/")
+    navigate("/");
   }
 
-  function onChange(e){
-setFormData(prevState => ({
-  ...prevState,
-  [e.target.id] : e.target.value
-}))
+  function onChange(e) {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
   }
- async function onSubmit() {
-try {
-  if(auth.currentUser.displayName !== name){
- await updateProfile(auth.currentUser, {
-  displayName: name,
- })
+  async function onSubmit() {
+    try {
+      if (auth.currentUser.displayName !== name) {
+        await updateProfile(auth.currentUser, {
+          displayName: name,
+        });
 
- const docRef = doc(db, "user", auth.currentUser.uid)
+        const docRef = doc(db, "user", auth.currentUser.uid);
 
- await updateDoc(docRef,{
-  name,
- })
-  }
-  toast.success("profile details updated ")
-} catch (error) {
-  toast.error("could not update the profile details")
-}
+        await updateDoc(docRef, {
+          name,
+        });
+      }
+      toast.success("profile details updated ");
+    } catch (error) {
+      toast.error("could not update the profile details");
+    }
   }
   return (
     <>
@@ -57,7 +58,9 @@ try {
               value={name}
               onChange={onChange}
               disabled={!changeDetail}
-              className={`w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out mb-6 ${changeDetail && " bg-red-200 focus:bg-red-200"}`}
+              className={`w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out mb-6 ${
+                changeDetail && " bg-red-200 focus:bg-red-200"
+              }`}
             />
             <input
               type="email"
@@ -70,11 +73,14 @@ try {
             <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg">
               <p className="flex items-center mb-6">
                 Do you want to change your name?
-                <span className=" text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer" onClick={()=>{
-                  changeDetail && onSubmit()
-                  setChangeDetail(prevState => !prevState)}
- } >
-                  {changeDetail ? "Apply Changes": "Edit"}
+                <span
+                  className=" text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer"
+                  onClick={() => {
+                    changeDetail && onSubmit();
+                    setChangeDetail((prevState) => !prevState);
+                  }}
+                >
+                  {changeDetail ? "Apply Changes" : "Edit"}
                 </span>
               </p>
               <p
@@ -85,6 +91,12 @@ try {
               </p>
             </div>
           </form>
+          <button type="submit" className="w-full bg-blue-600 text-white uppercase py-3 px-7 text-sm font-medium rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800">
+            <Link to="/create-listing" className="flex justify-center items-center">
+              <FcHome className="mr-2 text-3xl bg-red-200 rounded-full border-2" />
+              Sell Or Rent Your Home
+            </Link>
+          </button>
         </div>
       </section>
     </>
